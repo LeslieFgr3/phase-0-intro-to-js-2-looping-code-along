6778be42608b2159f934668641a57c7beb99f3c6
Leslie <lesliefgr3@gmail.com>
2022-05-03 22:35:42 -0400

    commit tree 0b1f6cf21b3ac604d1673a64c3291cd3f314acd7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, gift) {
+    let thankYou = [];
+  let i = 0; // 
+  while (i < names.length) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return thankYou;
+}
+const countDown = ["10", "9", "8","7","6","5","4","3","2","1","0"];
+for (let countDown = 0; countDown < 10; countDown++) {
+    console.log(`${countDown}`);
+    
+  }
